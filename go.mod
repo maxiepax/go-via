3db04a148bae16634417652759256bdd0ab84419
Kim Johansson <kimjohansson@kjohansson-a01.vmware.com>
2021-02-17 08:58:41 +0100

    created go.mod, main.go

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/maxiepax/go-via
+
+go 1.15
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/fatih/camelcase v1.0.0 // indirect
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/koding/multiconfig v0.0.0-20171124222453-69c27309b2d7 // indirect
+	github.com/sirupsen/logrus v1.7.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	honnef.co/go/tools v0.1.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,49 @@
+//go:generate bash -c "go get github.com/swaggo/swag/cmd/swag && swag init"
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/koding/multiconfig"
+	"github.com/sirupsen/logrus"
+	"honnef.co/go/tools/config"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
+// @title go-via
+// @version 0.1
+// @description VMware Imaging Appliances written in GO with full HTTP-REST
+
+// @BasePath /v1
+
+func main() {
+
+	//setup logging
+	logrus.WithFields(logrus.Fields{
+		"version": version,
+		"commit":  commit,
+		"date":    date,
+	}).Infof("Startup")
+
+	//enable config
+
+	d := multiconfig.New()
+
+	conf := new(config.Config)
+
+	err := d.Load(conf)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"err": err,
+		}).Fatalf("failed to load config")
+	}
+
+	fmt.Println("hello world")
+
+}
