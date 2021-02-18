1dcce17693e3460f88b67f94069a69152da235e3
Kim Johansson <kimjohansson@kjohansson-a01.vmware.com>
2021-02-18 19:52:37 +0100

    added groups model

--- models/address.go
+++ models/address.go
@@ -5,17 +5,12 @@ import (
 )
 
 type AddressForm struct {
-	IP             string    `json:"ip_address" gorm:"type:varchar(15);not null;index:uniqIp,unique"`
-	Mac            string    `json:"mac" gorm:"type:varchar(17);not null"`
-	Hostname       string    `json:"hostname" gorm:"type:varchar(255)"`
-	Reserved       bool      `json:"reserved" gorm:"type:bool;index:uniqIp,unique"`
-	PoolID         NullInt32 `json:"pool_id" gorm:"type:BIGINT" swaggertype:"integer"`
-	AuthorizedVlan NullInt32 `json:"authorized_vlan" gorm:"type:SMALLINT" swaggertype:"integer"`
-	ManagedRef     string    `json:"managed_reference"`
-	// Host parameters
-	Fqdn     string `json:"fqdn" gorm:"type:varchar(255)"`
-	Password string `json:"password" gorm:"type:varchar(255)"`
-	ImageID  int    `json:"image_id gorm:"type:int(8)"`
+	IP         string    `json:"ip_address" gorm:"type:varchar(15);not null;index:uniqIp,unique"`
+	Mac        string    `json:"mac" gorm:"type:varchar(17);not null"`
+	Hostname   string    `json:"hostname" gorm:"type:varchar(255)"`
+	Reserved   bool      `json:"reserved" gorm:"type:bool;index:uniqIp,unique"`
+	PoolID     NullInt32 `json:"pool_id" gorm:"type:BIGINT" swaggertype:"integer"`
+	ManagedRef string    `json:"managed_reference"`
 }
 
 type Address struct {
---
+++ models/group.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"time"
+)
+
+type GroupForm struct {
+	DNS      string `json:"dns" gorm:"type:varchar(255)"`
+	NTP      string `json:"ntp" gorm:"type:varchar(255)"`
+	Password string `json:"password" gorm:"type:varchar(255)"`
+	ImageID  string `json:"image_id" gorm:"type:BIGINT"`
+}
+
+type GroupClass struct {
+	ID int `json:"id" gorm:"primary_key"`
+
+	GroupForm
+
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+}
