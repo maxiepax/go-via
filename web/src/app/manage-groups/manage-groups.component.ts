5e17c49ced263994c9500914dd8fe3e25296a83f
Kim Johansson <kjohansson@vmware.com>
2021-02-27 23:18:47 +0100

    added accordian to groups frontend

--- web/src/app/manage-groups/manage-groups.component.ts
+++ web/src/app/manage-groups/manage-groups.component.ts
@@ -2,6 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { cloudIcon, ClarityIcons } from '@cds/core/icon';
+import '@cds/core/icon/register.js';
+import '@cds/core/accordion/register.js';
+import '@cds/core/alert/register.js';
+import '@cds/core/button/register.js';
+import '@cds/core/checkbox/register.js';
+import '@cds/core/datalist/register.js';
+import '@cds/core/file/register.js';
+import '@cds/core/forms/register.js';
+import '@cds/core/input/register.js';
+import '@cds/core/password/register.js';
+import '@cds/core/radio/register.js';
+import '@cds/core/range/register.js';
+import '@cds/core/search/register.js';
+import '@cds/core/select/register.js';
+import '@cds/core/textarea/register.js';
+import '@cds/core/time/register.js';
+import '@cds/core/toggle/register.js';
 
 @Component({
   selector: 'app-manage-groups',
@@ -14,6 +32,8 @@ export class ManageGroupsComponent implements OnInit {
 	errors;
   groups;
 	form: FormGroup;
+  show = false;
+  panel1Expanded = true;
 
   constructor(private apiService: ApiService, private formBuilder: FormBuilder) {
   	this.form = this.formBuilder.group({
@@ -70,5 +90,8 @@ export class ManageGroupsComponent implements OnInit {
       });
       });
     }
-
+  
+    expandedChange(event): void {
+      this.panel1Expanded = event.detail;
+    }
 }
