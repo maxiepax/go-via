b35d91405078a6422236526bf785f2001c6da48e
Kim Johansson <kjohansson@vmware.com>
2021-04-06 09:23:24 +0200

    debugging boot.cfg

--- tftpd.go
+++ tftpd.go
@@ -66,7 +66,7 @@ func readHandler(filename string, rf io.ReaderFrom) error {
 			ip: "requesting mboot.efi",
 		}).Info("tftpd")
 		filename, _ = mbootPath(image.Path)
-	} else if strings.ToLower(filename) == "/boot.cfg" {
+	} else if strings.ToLower(filename) == "boot.cfg" {
 		//if the filename is boot.cfg, we serve the boot cfg that belongs to that build.
 		logrus.WithFields(logrus.Fields{
 			ip: "requesting boot.cfg",
