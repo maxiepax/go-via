10224d1e3f6a52707ea9c3084a709a81befd7897
Kim Johansson <kjohansson@vmware.com>
2021-04-25 22:18:19 +0200

    testing websockets for logs

--- web/src/app/logs/logs.component.ts
+++ web/src/app/logs/logs.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {webSocket, WebSocketSubject} from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-logs',
@@ -8,23 +7,20 @@ import {webSocket, WebSocketSubject} from 'rxjs/webSocket';
 })
 export class LogsComponent implements OnInit {
 
-  myWebSocket: WebSocketSubject<any> = webSocket('ws://' +  window.location.hostname + ':8080/v1/log');
+  data;
 
   constructor() {
-    this.myWebSocket.subscribe(
-      msg => console.log('message received: ' + msg),
-      // Called whenever there is a message from the server
-      err => console.log(err),
-      // Called if WebSocket API signals some kind of error
-      () => console.log('complete')
-      // Called when connection is closed (for whatever reason)
-   );
+    const ws = new Websocket('ws://' +  window.location.hostname + ':8080/v1/log')
+    ws.addEventListener('message', event => {
+      console.log(event.data);
+    })
   }
 
   ngOnInit(): void {
 
-
   }
 }
 
+
+// 
 
