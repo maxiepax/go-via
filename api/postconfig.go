0592d8a9b74faa64e9aacd5aa65bd696337bd9fd
Kim Johansson <kjohansson@vmware.com>
2021-07-30 23:21:15 +0200

    testing retry

--- api/postconfig.go
+++ api/postconfig.go
@@ -97,7 +97,11 @@ func ProvisioningWorker(item models.Address) {
 		}).Info(item.IP)
 	}
 
-	retryer := vim25.Retry(c.RoundTripper, vim25.RetryTemporaryNetworkError, 3)
+	func CustomRetryTemporaryNetworkError(err error) (bool, time.Duration) {
+		return vim25.IsTemporaryNetworkError(err), time.Second * 5
+	}
+	
+	retryer := vim25.Retry(c.RoundTripper, CustomRetryTemporaryNetworkError, 3)
 	c.RoundTripper = retryer
 
 	// since we're always going to be talking directly to the host, dont asume connection through vCenter.
