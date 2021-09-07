6bb8fb62da399608f6309bb6082af5ca566d2905
Kim Johansson <kjohansson@vmware.com>
2021-09-07 21:08:46 +0200

    cleaned up unneeded dependencies and debug info

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,28 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { startWith } from 'rxjs/operators';
-import { Observable } from 'rxjs';
 import { ApiService } from './api.service';
 
-/*
-import { cloudIcon, ClarityIcons } from '@cds/core/icon';
-import '@cds/core/icon/register.js';
-import '@cds/core/accordion/register.js';
-import '@cds/core/alert/register.js';
-import '@cds/core/button/register.js';
-import '@cds/core/checkbox/register.js';
-import '@cds/core/datalist/register.js';
-import '@cds/core/file/register.js';
-import '@cds/core/forms/register.js';
-import '@cds/core/input/register.js';
-import '@cds/core/password/register.js';
-import '@cds/core/radio/register.js';
-import '@cds/core/range/register.js';
-import '@cds/core/search/register.js';
-import '@cds/core/select/register.js';
-import '@cds/core/textarea/register.js';
-import '@cds/core/time/register.js';
-import '@cds/core/toggle/register.js'; */
 
 @Component({
   selector: 'app-root',
@@ -39,7 +17,6 @@ export class AppComponent {
   ngOnInit(): void {
     this.apiService.getVersion().subscribe((data: any) => {
       this.version = data;
-      console.log(this.version);
     });
 
   }
