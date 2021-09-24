29fa032925b12bfde4c6e71b56b67a8e2dbe5028
Kim Johansson <kjohansson@vmware.com>
2021-09-24 23:08:45 +0200

    added temp debugging

--- api/postconfig.go
+++ api/postconfig.go
@@ -303,7 +303,7 @@ func ProvisioningWorker(item models.Address, key string) {
 	if item.Group.Vlan != "" {
 		//if vlan is set, configure the "VM Network" portgroup with the same vlanid.
 		cmd := strings.Fields("network vswitch standard portgroup set --vlan-id " + item.Group.Vlan)
-		cmd = append(cmd, " -p VM Network")
+		cmd = append(cmd, " -p 'VM Network'")
 		spew.Dump(cmd)
 		_, err := e.Run(cmd)
 		if err != nil {
